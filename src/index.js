55efb8786b119615e0d56b40ca84e033c1276727
prateekpadile <prateekpadile@gmail..com>
2025-07-26 15:51:27 +0530

    Util code

--- src/index.js
+++ src/index.js
@@ -7,3 +7,11 @@ dotenv.config({
 });
 
  connectDB()
+ .then( () => {
+    app.listen(process.env.PORT  || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+ })
+ .catch((error) => { 
+     console.error("Failed to connect to the database:", error);
+ });
